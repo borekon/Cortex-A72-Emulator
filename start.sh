915175c251a0d3efdceecf0cfb02f00841a881fa
Alfonso <vivancos@outlook.es>
2023-06-29 12:44:21 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 docker run -ti --rm \
 	--mount type=bind,src=i$(pwd)/images,dst=/emu \
-    -e MEMORY=1G \
+    -e MEMORY=2G \
+    -e NUM_CPUS=1 \
+    -e CPU_CORES=4 \    
     -p 5555:22 \
     ljishen/cortex-a72-emulator
 
